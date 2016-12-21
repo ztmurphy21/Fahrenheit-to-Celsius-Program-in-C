02b864d77a154d69200af3d92dcd2fc7861e2233
Zachary Murphy <murphyz@my.wccc.edu>
2016-12-20 22:38:30 -0500

    using symbolic constatns in c
    used for
    LOWER
    UPPER
    and
    STEP

--- Temperature Program/main.c
+++ Temperature Program/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LOWER 0 /*lower limit of the table*/
+#define UPPER 300 /*higher limit of the table*/
+#define STEP 20 /*step size of table*/
+
 /*print Fahrenheit-Celsius table
 for f= 0, 20...., 300*/
 main()
@@ -8,7 +12,7 @@ main()
     int fahrTemp;
     printf("Fahrenheit to Celsius Table.");
 
-    for(fahrTemp = 320; fahrTemp >= 0; fahrTemp = fahrTemp - 20){
+    for(fahrTemp = LOWER; fahrTemp <= UPPER; fahrTemp = fahrTemp + STEP){
         printf("%3d %6.1f\n", fahrTemp, (5.0/9.0) * (fahrTemp-32));
     }
 
