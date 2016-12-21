d11d66c7f7f8565a8db2f8a50c3f18f2c484a638
Zachary Murphy <murphyz@my.wccc.edu>
2016-12-20 22:24:33 -0500

    using a for loop
    makes coding life alot shorter...

--- Temperature Program/main.c
+++ Temperature Program/main.c
@@ -5,25 +5,9 @@
 for f= 0, 20...., 300*/
 main()
 {
-    int fahrTemp, celTemp;
+    int fahrTemp;
 
-    /* lower limit of table*/
-    int lower = 0;
-
-    /*upper limit of table*/
-    int upper = 300;
-
-    /* step size for table*/
-    int step = 20;
-
-    fahrTemp = lower;
-    printf("Fahrenheit-Celsius Table\n");
-
-    while (fahrTemp <= upper){
-            celTemp = 5 * (fahrTemp-32) / 9;
-    printf("%3d %6d\n", fahrTemp, celTemp);
-    fahrTemp = fahrTemp + step;
-
-    }
+    for(fahrTemp = 0; fahrTemp <= 300; fahrTemp = fahr + 20)
+        printf("%3d %6.1f\n", fahrTemp, (5.0/9.0) * (fahrTemp-32));
 
 }
