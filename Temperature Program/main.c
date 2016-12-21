1aeed82a2a8927aaf0fa53f4e69f27bbf78a3b0e
Zachary Murphy <murphyz@my.wccc.edu>
2016-12-20 22:28:14 -0500

    one fix

--- Temperature Program/main.c
+++ Temperature Program/main.c
@@ -7,7 +7,7 @@ main()
 {
     int fahrTemp;
 
-    for(fahrTemp = 0; fahrTemp <= 300; fahrTemp = fahr + 20)
+    for(fahrTemp = 0; fahrTemp >= 300; fahrTemp = fahrTemp + 20)
         printf("%3d %6.1f\n", fahrTemp, (5.0/9.0) * (fahrTemp-32));
 
 }
