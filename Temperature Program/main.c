ada6e47d40c67d59e4182c25c34987e5861ae484
Zachary Murphy <murphyz@my.wccc.edu>
2016-12-20 22:33:22 -0500

    can't forget the title.

--- Temperature Program/main.c
+++ Temperature Program/main.c
@@ -6,8 +6,10 @@ for f= 0, 20...., 300*/
 main()
 {
     int fahrTemp;
+    printf("Fahrenheit to Celsius Table.");
 
-    for(fahrTemp = 0; fahrTemp >= 300; fahrTemp = fahrTemp + 20)
+    for(fahrTemp = 0; fahrTemp <= 300; fahrTemp = fahrTemp + 20){
         printf("%3d %6.1f\n", fahrTemp, (5.0/9.0) * (fahrTemp-32));
+    }
 
 }
