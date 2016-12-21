573bcce5a28866e26b3fd3557f915ba90cb14841
Zachary Murphy <murphyz@my.wccc.edu>
2016-12-20 22:34:56 -0500

    lets reverse the order
    reversing the order in the for loop

--- Temperature Program/main.c
+++ Temperature Program/main.c
@@ -8,7 +8,7 @@ main()
     int fahrTemp;
     printf("Fahrenheit to Celsius Table.");
 
-    for(fahrTemp = 0; fahrTemp <= 300; fahrTemp = fahrTemp + 20){
+    for(fahrTemp = 320; fahrTemp >= 0; fahrTemp = fahrTemp - 20){
         printf("%3d %6.1f\n", fahrTemp, (5.0/9.0) * (fahrTemp-32));
     }
 
